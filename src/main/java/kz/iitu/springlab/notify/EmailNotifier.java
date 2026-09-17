a5116eed974501535a6f4cb02fb5d45e829e5879
Aimar Ershat <student@iitu.edu.kz>
2026-09-18 03:21:01 +0500

    lab01: add REST controller with /api/hello and /api/info

---
+++ src/main/java/kz/iitu/springlab/notify/EmailNotifier.java
@@ -0,0 +1,20 @@
+package kz.iitu.springlab.notify;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component("email")
+@Order(2)
+public class EmailNotifier implements Notifier {
+
+    @Override
+    public String send(String message) {
+        return "email: " + message;
+    }
+
+    @Override
+    public String channel() {
+        return "email";
+    }
+}
